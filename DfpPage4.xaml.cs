0766bf1ac8eeb4b2e5b3f3a10c1b11a222b2d408
Samuel-Jaja <66678860+Samuel-Jaja@users.noreply.github.com>
2020-09-05 22:25:39 +0100

    Add files via upload
    Xamarin Project on POAST Suite 1.0 <Mobile Development >

---
+++ DfpPage4.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PoastSuite
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DfpPage4 : ContentPage
+    {
+        public DfpPage4()
+        {
+            InitializeComponent();
+        }
+
+        private void NextIterationButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        async private void Next2ndIterationButton_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new DfpPage5());
+        }
+
+       async private void BackButtonIterationTwoPage_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new DfpPage3());
+        }
+    }
+}
